70541657c654dd4e613992562b7cf34bb251f551
doehm <danieloehm@gmail.com>
2019-08-21 22:36:05 +1000

    Adding ALS and other updates

--- R/als.R
+++ R/als.R
@@ -1,32 +1,29 @@
-#' @title Matrix factorisation
-#' @description Applies matrix factorisation by gradient descent (Funk SVD) to a given matrix with missing values
+#' @title Alternating Least Squares
+#' @description Implements alternating least squares matrix factorisation on a given matrix
 #' @param Y Matrix with missing values. Missing values should be \code{NA}
 #' @param k_dim Number of latent variables
 #' @param test The select test. See \code{select_test}
 #' @param epochs Maximum iterations
-#' @param lr Learning rate
 #' @param lambda Regularisation parameter
 #' @param tol Iteration tolerance
 #' @param pbar Progress bar toggle
-#' @details Implements matrix factorisation by gradient descent. Matrix \code{Y} should have \code{NA}'s as missing values. The matrix will be factorised
+#' @details Implements Alternating least squares. Matrix \code{Y} should have \code{NA}'s as missing values. The matrix will be factorised
 #' into two matrices U and V, the user and feature matrix. The U matrix essentially contains weight each user gives to a certain feature. The
 #' function will output the accuracy of the selected test sample by using \code{test}. See \code{select_test} for more information.
-#' @rdname matfact
 #' @importFrom progress progress_bar
 #' @importFrom stats rnorm
 #' @examples
 #' \dontrun{
-#' m <- matrix(sample(c(NA, 1:5), 60, replace = TRUE, prob = c(0.2, rep(0.8/5, 5))), nrow = 10)
-#' id <- select_test(m, 0.2)
-#' mf <- matrix_factorisation(m, 2, test = id$test)
+#' m <- matrix(sample(1:5, 60, replace = TRUE, nrow = 10)
+#' mf <- als(m, 2)
 #' mf$pred
 #' }
 #' @export
 
 
+# als
 
-# matrix factorisation function
-matrix_factorisation <- function(Y, k_dim, test = NULL, epochs = 500, lr = 0.001, lambda = 0.02, tol = 1e-6, pbar = FALSE) {
+als <- function(Y, k_dim, test = NULL, epochs = 500, lambda = 0.01, tol = 1e-6, pbar = FALSE){
 
   # set train
   Y_train <- Y
@@ -53,6 +50,7 @@ matrix_factorisation <- function(Y, k_dim, test = NULL, epochs = 500, lr = 0.001
     format = ":elapsedfull // dimensions :kdim // epoch :epoch // train :trainrmse // test :testrmse // delta :delta",
     clear = FALSE, total = NA)
 
+  # als
   while ((k_epoch <= epochs & tol < delta)) {
 
     for (latent_var in 1:k_dim) {
@@ -60,13 +58,13 @@ matrix_factorisation <- function(Y, k_dim, test = NULL, epochs = 500, lr = 0.001
       # loop through each item where there exists a rating and update U
       for (j in 1:ncol(Y)) {
         ur <- observed_user_list[[j]]
-        U[ur, latent_var] <- U[ur, latent_var] + lr*(error[ur, j]*V[j, latent_var] - lambda*U[ur, latent_var])
+        V[j, latent_var] <- (solve(t(U) %*% U) %*% t(U))[latent_var, ur] %*% as.matrix(Y_train[ur, j])
       }
 
       # loop through each user where there exists a rating and update V
       for (i in 1:nrow(Y)) {
         ir <- observed_item_list[[i]]
-        V[ir, latent_var] <- V[ir, latent_var] + lr*(error[i, ir]*U[i, latent_var] - lambda*V[ir, latent_var])
+        U[i, latent_var] <- (solve(t(V) %*% V) %*% t(V))[latent_var, ir] %*% as.matrix(Y_train[i, ir])
       }
 
       error <- (Y_train - U %*% t(V))
@@ -77,7 +75,7 @@ matrix_factorisation <- function(Y, k_dim, test = NULL, epochs = 500, lr = 0.001
       if(is.nan(rmse[k])) stop("gradient explosion - try smaller learning rate")
 
       if(pbar) pb$tick(tokens = list(kdim = k_dim, epoch = k_epoch, trainrmse = format(round(rmse[k], 4), nsmall = 4),
-                            testrmse = format(round(rmse_test[k], 4), nsmall = 4), delta = format(delta, digits = 4, nsmall = 4, scientific = TRUE)
+                                     testrmse = format(round(rmse_test[k], 4), nsmall = 4), delta = format(delta, digits = 4, nsmall = 4, scientific = TRUE)
       ))
     }
     k_epoch <- k_epoch + 1
@@ -85,3 +83,4 @@ matrix_factorisation <- function(Y, k_dim, test = NULL, epochs = 500, lr = 0.001
   cat("\n")
   return(list(pred = U %*% t(V), u = U, v = V, k_dim = k_dim, epochs = epochs, train = rmse, test = rmse_test, delta = delta))
 }
+
--- R/select-test.R
+++ R/select-test.R
@@ -2,6 +2,7 @@
 #' @description Select a test set to pass to \code{matrix_factorisation}
 #' @param Y Matrix with missing values. Missing values should be \code{NA}
 #' @param r Ratio i.e. proption of observations held out for testing
+#' @param min_row The minimum observations to leave in each row. This protects against leaving an entire row missing.
 #' @details Implements matrix factorisation by gradient descent. Matrix \code{Y} should have \code{NA}'s as missing values. The matrix will be factorised
 #' into two matrices U and V, the user and feature matrix. The U matrix essentially contains weight each user gives to a certain feature. The
 #' function will output the accuracy of the selected test sample by using \code{test}. See \code{select_test} for more information.
@@ -15,9 +16,25 @@
 #' }
 #' @export
 
-select_test <- function(Y, r){
-  id <- sample(which(!is.na(Y)), length(which(!is.na(Y)))*r)
+select_test <- function(Y, r, min_row = 1){
+
+  # hold out min_row observations from each row
+  hold_out <- lapply(1:nrow(Y), function(x) sample(which(!is.na(Y[x,])), min(min_row, length(which(!is.na(Y[x,]))))))
+
+  # replace hold_out with NA's so they don't get selected
   train <- Y
+  for(k in 1:nrow(Y)){
+    train[k,hold_out[[k]]] <- NA
+  }
+
+  # select test
+  id <- sample(which(!is.na(Y)), length(which(!is.na(Y)))*r)
   train[id] <- NA
+
+  # put back hold out
+  for(k in 1:nrow(Y)){
+    train[k,hold_out[[k]]] <- Y[k,hold_out[[k]]]
+  }
+
   list(train = train, test = id)
 }
